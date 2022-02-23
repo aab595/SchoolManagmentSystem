31d84bef70737c52d5a34135b99395dd428b5fb1
Abdoul Aziz BA <aaziz.ba2@univ-thies.sn>
2022-02-23 13:42:29 +0100

    Reorder app.js file content

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 require('dotenv').config();
-const express      = require('express');
-const cookieParser = require('cookie-parser')
-const connectDB    = require('./src/database/connection');
-const mainRouter   = require('./src/routes/mainRouter');
-const userRouter   = require('./src/routes/userRoute');
-const authRouter   = require('./src/routes/authRouter');
-const classeRouter = require('./src/routes/classeRoute');
-const classroomRouter = require('./src/routes/classroomRoute');
-const courseRouter = require('./src/routes/courseRoute');
-const gradeRouter = require('./src/routes/gradeRoute');
-const subjectRouter = require('./src/routes/subjectRoute');
+const express          = require('express');
+const cookieParser     = require('cookie-parser')
+const connectDB        = require('./src/database/connection');
+const mainRouter       = require('./src/routes/mainRouter');
+const userRouter       = require('./src/routes/userRoute');
+const authRouter       = require('./src/routes/authRouter');
+const classeRouter     = require('./src/routes/classeRoute');
+const classroomRouter  = require('./src/routes/classroomRoute');
+const courseRouter     = require('./src/routes/courseRoute');
+const gradeRouter      = require('./src/routes/gradeRoute');
+const subjectRouter    = require('./src/routes/subjectRoute');
 const timeTablesRouter = require('./src/routes/timeTablesRoute');
-const authenticate = require('./src/middleware/Authenticate');
-const PORT         = process.env.PORT || 3000;
-const HOST         = process.env.HOST;
+const authenticate     = require('./src/middleware/Authenticate');
+const PORT             = process.env.PORT || 3000;
+const HOST             = process.env.HOST;
 
 app = express();
-app.use(cookieParser())
 
+app.use(cookieParser())
 app.use(express.json()) // for json
-   app.use(express.urlencoded({ extended: true })) // for form data
-// Set view engine
-app.set('views', './views');
+app.use(express.urlencoded({ extended: true })) // for form data
+app.set('views', './views'); // Set view engine
 app.set('view engine', 'ejs');
-// Load assets
-app.use(express.static('public'));
+app.use(express.static('public')); // Load assets
+
 // Load routers
 app.use('/', mainRouter);
 app.use('/admin', userRouter);
@@ -35,7 +34,9 @@ app.use('/course', courseRouter);
 app.use('/grade', gradeRouter);
 app.use('/subject', subjectRouter);
 app.use('/timeTables', timeTablesRouter);
+
 // Database connection
 connectDB();
+
 // Start server
 app.listen(PORT, () => { console.log(`Listening on http://${HOST}:${PORT}`) })
